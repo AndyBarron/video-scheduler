09fedf91f9db88acdafb87fc75d3bedc0759fd35
Andy Barron <AndrewLBarron@gmail.com>
2018-01-01 16:28:13 -0600

    slim navbar

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Nav extends React.Component {
   render() {
     return (
-      <nav className={`navbar ${this.props.className || ''}`}>
+      <nav className={`navbar p-1 ${this.props.className || ''}`}>
         <div className="container">
           <a className="navbar-brand" href="/">Video Scheduler</a>
         </div>
